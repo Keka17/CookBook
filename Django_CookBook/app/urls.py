5b24f005d29017b377ec7d61394874682ece42c3
Victoria <vihalfblood@gmail.com>
2025-08-13 17:11:55 +0300

    Шаблон поисковой страницы

--- Django_CookBook/app/urls.py
+++ Django_CookBook/app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import BestRecipes, recipe, rate_recipe
+from .views import BestRecipes, SearchRecipes, recipe, rate_recipe
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="main.html"), name="main"),
     path("best", BestRecipes.as_view(), name="best"),
     path("recipe/<int:pk>", recipe, name="recipe_detail"),
     path("recipe/<int:pk>/rate/", rate_recipe, name="rate_recipe"),
+    path("search", SearchRecipes.as_view(), name="recipe_search")
 
 ]
--- Django_CookBook/app/views.py
+++ Django_CookBook/app/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect
 from .models import Recipe, User, Category, RecipeRating
 from django.shortcuts import render, get_object_or_404
@@ -5,23 +6,28 @@ from django.views.generic import (ListView, DetailView,
                                   CreateView, UpdateView, DeleteView, TemplateView)
 from django.db.models import Avg, Count
 from django.contrib.auth.decorators import login_required
+from django.db.models.functions import Lower
 
 
 class BestRecipes(ListView):
-    """Страница с лучиши рецептами"""
+    """Страница с лучшими рецептами"""
     model = Recipe
     template_name = "best.html"
     context_object_name = "recipes"
     paginate_by = 6
 
     def get_queryset(self):
-        """Фильтрация по категориям"""
+        """Фильтрация рецептов с рейтингом >= 4.7.
+        Сортировка по убыванию; возможность фильтрации по категориям"""
+
+        # Базовый queryset с аннотациями среднего рейтинга и количества оценок
         queryset = super().get_queryset().annotate(
             average_rating=Avg("ratings__rating"),
             rating_count=Count("ratings")
         ).filter(
             average_rating__gte=4.7).order_by("-average_rating")
 
+        # Фильтрацию по категориям через GET-запрос
         category_id = self.request.GET.get("category")
         if category_id:
             queryset = queryset.filter(category__id=category_id)
@@ -29,39 +35,85 @@ class BestRecipes(ListView):
         return queryset
 
     def get_context_data(self, **kwargs):
-        """Добавление категорий в контекст для фильтрации"""
+        """Добавление всех категорий в контекст для фильтрации"""
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         return context
 
 
+class SearchRecipes(ListView):
+    """Класс поиска рецептов"""
+    model = Recipe
+    template_name = "search.html"
+    context_object_name = "recipes"
+    paginate_by = 6
+
+    def get_queryset(self):
+        """Функция возвращает queryset рецептов, отфильтрованных по
+        названию блюда и категории (выбор через sidebar)"""
+        queryset = Recipe.objects.all()
+
+        # Получение данных из GET-запроса
+        query = self.request.GET.get("q", "").strip()  # Удаление пробелов в поисковом запросе
+        category_id = self.request.GET.get("category")
+
+        # Фильтрация по названию
+        if query:
+            queryset = queryset.filter(dish_name__icontains=query)
+
+        # Фильтрация по категории
+        if category_id:
+            queryset = queryset.filter(category__id=category_id)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        """Добавление в контекст всех категорий, поисковой запрос
+        и выбранную категорию"""
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['search_query'] = self.request.GET.get('q', '')
+        context['selected_category'] = self.request.GET.get('category', '')
+
+        return context
+
+
 def recipe(request, pk):
-    """Конкретный рецепт"""
-    recipe = get_object_or_404(Recipe, pk=pk)
+    """Конкретный рецепт; авторизованные пользователи
+    видят свою оценку рецепта"""
+    recipe = get_object_or_404(Recipe, pk=pk)  # Безопасное извлечение объекта
 
     user_rating = None
+
+    # Провека авторизациид для показа оценки
     if request.user.is_authenticated:
         try:
             rating_obj = RecipeRating.objects.get(user=request.user, recipe=recipe)
             user_rating = int(rating_obj.rating) if rating_obj else None
-
         except RecipeRating.DoesNotExist:
             pass
 
     context = {
         "recipe": recipe,
-        "user_rating": user_rating,
+        "user_rating": user_rating,  # Оценка текущего пользователя
     }
 
     return render(request, "recipe.html", context)
 
 @login_required
 def rate_recipe(request, pk):
+    """Обработка оценки рецепта авторизованным пользователем;
+    функция создает или обновляет оценку"""
     recipe = get_object_or_404(Recipe, pk=pk)
+
+    # Получение оценки из POST-запроса
     rating_value = int(request.POST.get("rating"))
+
+    # Создание/обновление оценки
     rating_obj, created = RecipeRating.objects.update_or_create(
         user=request.user, recipe=recipe,
         defaults={"rating": rating_value}
     )
 
+    # Перенаправление на страницу рецепта
     return redirect('recipe_detail', pk=recipe.pk)
